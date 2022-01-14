5a2713d049b05967f5195218ea5d681068b9147e
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-14 16:22:45 -0300

    Iniciando o Exercicio de fixação sobre herança e polimorfismo da seção 10 do curso do prof Nélio alves

---
+++ ExFixHerancaPolimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExFixHerancaPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
