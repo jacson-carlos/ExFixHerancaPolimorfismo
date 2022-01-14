af990f297278576ce628bddc8980c656ce4ba77d
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-14 16:37:46 -0300

    Implementada a ImportedProduct que é SubClasse de product

---
+++ ExFixHerancaPolimorfismo/Entities/ImportedProduct.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+namespace ExFixHerancaPolimorfismo.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) 
+            : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag() + $" (Customs fee: 4 {CustomsFee.ToString("F2", CultureInfo.InvariantCulture )})";
+        }
+    }
+}
