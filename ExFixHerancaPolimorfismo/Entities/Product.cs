00fca567b8e9d3d836ac83dd956e9be059c494af
Jacson Carlos <jacson_carlos@hotmail.com>
2022-01-14 16:30:44 -0300

    Implementada a classe Product

---
+++ ExFixHerancaPolimorfismo/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace ExFixHerancaPolimorfismo.Entities
+{
+    class Product
+    {
+        public string Name{ get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Name
+                + " $ "
+                + Price;
+        }
+    }
+}
